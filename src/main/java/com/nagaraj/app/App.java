0612a87fa8082be3a092305fa12214c3aaeac44e
Nagaraj02hp <n.nagaraj88@gmail.com>
2018-07-24 12:03:46 +0530

    adding my java project

---
+++ src/main/java/com/nagaraj/app/App.java
@@ -0,0 +1,13 @@
+package com.nagaraj.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
